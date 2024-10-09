77f898675f89cf510edfc5b42e94f73b3e971cd7
Amogh Bansal <bansalamogh0@gmail.com>
2024-10-09 15:44:42 +0530

    Learning Portal on Cloud

--- lib/learnalphabet.dart
+++ lib/learnalphabet.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show ByteData, rootBundle;
+import 'package:flutter/services.dart' show ByteData, NetworkAssetBundle, rootBundle;
 import 'dart:typed_data';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -85,8 +85,8 @@ class _LearnAlphabetState extends State<LearnAlphabet> {
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
             colors: [
-             Color.fromARGB(255, 219, 69, 249),
-            Color.fromARGB(255, 135, 205, 238),
+              Color.fromARGB(255, 219, 69, 249),
+              Color.fromARGB(255, 135, 205, 238),
               Colors.white
             ],
             stops: [0.0, 0.5, 1.0],
@@ -115,14 +115,72 @@ class AlphabetLearn extends StatefulWidget {
   State<AlphabetLearn> createState() => _AlphabetLearnState();
 }
 
-class _AlphabetLearnState extends State<AlphabetLearn>
-    with SingleTickerProviderStateMixin {
+class _AlphabetLearnState extends State<AlphabetLearn> with SingleTickerProviderStateMixin {
   List<String> alphabetImages = [
     'A', 'B', 'C', 'D', 'E', 'F', 'G',
     'H', 'I', 'J', 'K', 'L', 'M', 'N',
     'O', 'P', 'Q', 'R', 'S', 'T', 'U',
     'V', 'W', 'X', 'Y', 'Z'
   ];
+
+  // Map to store Cloudinary URLs for each alphabet (PNG and GIF)
+  final Map<String, String> alphabetGifs = <String, String>{
+    'A': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728462628/A.gif',
+    'B': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728462629/B.gif',
+    'C': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728462532/C.gif',
+    'D': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728462636/D.gif',
+    'E': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728462667/E.gif',
+    'F': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728462679/F.gif',
+    'G': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728462662/G.gif',
+    'H': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728462529/H.gif',
+    'I': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728462650/I.gif',
+    'J': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728462530/J.gif',
+    'K': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728462524/K.gif',
+    'L': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728462524/L.gif',
+    'M': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728462526/M.gif',
+    'N': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728462524/N.gif',
+    'O': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728462525/O.gif',
+    'P': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728462525/P.gif',
+    'Q': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728462636/Q.gif',
+    'R': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728462526/R.gif',
+    'S': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728462647/S.gif',
+    'T': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728462642/T.gif',
+    'U': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728462624/U.gif',
+    'V': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728462625/V.gif',
+    'W': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728462625/W.gif',
+    'X': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728462626/X.gif',
+    'Y': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728462625/Y.gif',
+    'Z': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728462628/Z.gif',
+  };
+
+  final Map<String, String> alphabetPngs = {
+    'A': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728467603/A1_gfr4h2.png',
+    'B': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728467603/B1_vwajxd.png',
+    'C': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728467603/C1_rretlt.png',
+    'D': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728467604/D1_ph45fg.png',
+    'E': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728467604/E1_uabvnl.png',
+    'F': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728467604/F1_vqaepw.png',
+    'G': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728467604/G1_tearss.png',
+    'H': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728467604/H1_hqygpx.png',
+    'I': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728467605/I1_vfzfjq.png',
+    'J': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728467606/J1_rndtny.png',
+    'K': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728467605/K1_fm574r.png',
+    'L': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728467605/L1_igjjkc.png ',
+    'M': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728467605/M1_pxewpw.png',
+    'N': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728467606/N1_eefhuu.png',
+    'O': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728467606/O1_m520oa.png',
+    'P': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728467606/P1_btco85.png',
+    'Q': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728467611/Q1_vatfjd.png',
+    'R': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728467611/R1_git3ju.png',
+    'S': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728467601/S1_d6ktm6.png',
+    'T': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728467602/T1_fdtzcr.png',
+    'U': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728467602/U1_zahxvm.png',
+    'V': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728467602/V1_yb9ydj.png',
+    'W': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728467602/W1_fpvs98.png',
+    'X': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728467602/X1_pptmdb.png',
+    'Y': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728467602/Y1_uw14sf.png',
+    'Z': 'https://res.cloudinary.com/dfph32nsq/image/upload/v1728467603/Z1_tlgtdg.png',
+  };
 
   final ScrollController _scrollController = ScrollController();
   late AnimationController _controller;
@@ -150,7 +208,14 @@ class _AlphabetLearnState extends State<AlphabetLearn>
   }
 
   void _onScroll() {
-    // No popup functionality
+    // Add any scroll-specific functionality here
+  }
+
+  Future<Widget> _loadGif(String url) async {
+    return Image.network(
+      url,
+      fit: BoxFit.cover,
+    );
   }
 
   @override
@@ -178,6 +243,7 @@ class _AlphabetLearnState extends State<AlphabetLearn>
                           padding: const EdgeInsets.only(left: 8.0, right: 8.0),
                           child: Column(
                             children: [
+                              // Display GIF
                               Card(
                                 elevation: 6.0,
                                 color: Colors.white,
@@ -189,29 +255,32 @@ class _AlphabetLearnState extends State<AlphabetLearn>
                                   child: SizedBox(
                                     width: 300,
                                     height: 300,
-                                    child: FutureBuilder<Widget>(
-                                      future: _loadGif('images/Alphabets/$imageName.gif'),
-                                      builder: (context, snapshot) {
-                                        if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
-                                          return snapshot.data!;
-                                        } else {
-                                          return Container(
-                                            width: 300,
-                                            height: 300,
-                                            color: Colors.grey,
-                                            child: const Center(
-                                              child: CircularProgressIndicator(),
-                                            ),
-                                          );
-                                        }
-                                      },
+                                    child: Image.network(
+                                      alphabetGifs[imageName]!,
+                                      fit: BoxFit.cover,
                                     ),
                                   ),
                                 ),
                               ),
                               const SizedBox(height: 8.0),
-                              AlphabetPngCard(
-                                imageName: imageName,
+                              // Display PNG of the same size
+                              Card(
+                                elevation: 6.0,
+                                color: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                ),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                  child: SizedBox(
+                                    width: 300,
+                                    height: 300,
+                                    child: Image.network(
+                                      alphabetPngs[imageName]!,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
                               ),
                               const SizedBox(height: 16.0),
                             ],
@@ -223,46 +292,6 @@ class _AlphabetLearnState extends State<AlphabetLearn>
                 );
               },
             ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Future<Widget> _loadGif(String path) async {
-    final ByteData data = await rootBundle.load(path);
-    final Uint8List bytes = data.buffer.asUint8List();
-    return Container(
-      color: Colors.white,
-      child: Image.memory(
-        bytes,
-        fit: BoxFit.cover,
-      ),
-    );
-  }
-}
-
-class AlphabetPngCard extends StatelessWidget {
-  final String imageName;
-
-  const AlphabetPngCard({super.key, required this.imageName});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 300,
-      height: 300,
-      child: Card(
-        elevation: 6.0,
-        color: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15.0),
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(15.0),
-          child: Image.asset(
-            'images/Alphabets/$imageName.png',
-            fit: BoxFit.cover,
           ),
         ),
       ),
