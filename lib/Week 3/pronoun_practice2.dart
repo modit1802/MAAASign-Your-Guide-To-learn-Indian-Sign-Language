60ce06dff78fafdd856cdeedc93db1993fee8156
Amogh Bansal <bansalamogh0@gmail.com>
2024-11-26 15:28:58 +0530

    Bingo Tutorial for Week 3 Completed

--- lib/Week 3/pronoun_practice2.dart
+++ lib/Week 3/pronoun_practice2.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 import 'dart:math';
+import 'package:SignEase/Week 3/bingo_tutorial.dart';
 
 import 'bingo_result_pronoun.dart';
 
@@ -56,6 +57,7 @@ class _BingoScreenState extends State<BingoScreen> with SingleTickerProviderStat
   @override
   void initState() {
     super.initState();
+    _showTutorialScreen();
     _controller = AnimationController(vsync: this, duration: Duration(milliseconds: 500));
     _generateNewRound();
   }
@@ -138,13 +140,39 @@ class _BingoScreenState extends State<BingoScreen> with SingleTickerProviderStat
     _controller.dispose();
     super.dispose();
   }
-
+  void _showTutorialScreen() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) =>
+              BingoTutorialScreen(
+                onBackPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+        ),
+      );
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Video Bingo Game'),
         centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.help_outline),
+            onPressed: () {
+              // Navigate to the tutorial screen
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => BingoTutorialScreen(onBackPressed: () {Navigator.pop(context);  },)),
+              );
+            },
+          ),
+        ],
       ),
       body: FadeTransition(
         opacity: _controller,
@@ -194,9 +222,9 @@ class _BingoScreenState extends State<BingoScreen> with SingleTickerProviderStat
                       child: VideoTile(
                         key: ValueKey(noun),
                         url: pronounGifs[noun] ?? '',
-                        isCorrect: showAnswer && selectionStatus[noun] == true, // Check if this option is the correct answer
-                        isIncorrect: showAnswer && selectionStatus[noun] == false, // Check if this option was selected incorrectly
-                        isSelected: selectionStatus[noun] == false || selectionStatus[noun] == true, // Check if this option was selected
+                        isCorrect: showAnswer && selectionStatus[noun] == true,
+                        isIncorrect: showAnswer && selectionStatus[noun] == false,
+                        isSelected: selectionStatus[noun] == false || selectionStatus[noun] == true,
                         onSelected: (isSelected) {
                           if (isSelected) {
                             _checkAnswer(noun);
